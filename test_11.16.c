a10b0f0cfe77d1269d1fe907b21f412b1562d6fe
SOWJH <118143548+SOWJH@users.noreply.github.com>
2022-11-16 20:57:18 +0800

    Add files via upload

---
+++ test_11.16.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int i = 1;
+//
+//	while (i <= 10)
+//	{
+//		printf("%d ", i);
+//
+//		i++;
+//
+//	}
+//
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+//int main()
+//{
+//	int i = 0;
+//
+//	while (i <= 10)
+//	{
+//
+//		if (i == 5)
+//			continue;
+//		i++;
+//		printf("%d ", i);
+//	}
+//
+//	return 0;
+//}
+
+int main()
+{
+	int i =0  ;
+
+	while (i <= 10)
+	{
+		i++;
+		if (i == 5)
+			break;
+		printf("%d ", i);
+
+
+
+
+	}
+
+
+	return 0;
+}
+
+
+
+
+
+
