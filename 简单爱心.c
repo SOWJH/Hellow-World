c0e70a439327eefadfc1a5310d8550da884e280f
SOWJH <118143548+SOWJH@users.noreply.github.com>
2022-11-19 00:09:05 +0800

    Add files via upload

---
+++ 简单爱心.c
@@ -0,0 +1,110 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	int i, j, k, n = 0, x = 0, y = 50;
+
+	//爱心的头部无规律，所以直接打印
+
+	printf("\n\n\n\n");
+
+    printf("\n\n\n\n\n");
+    printf("         lovelove                   lovelove\n");
+    printf("       lovelovelove               lovelovelove\n");
+    printf("     lovelovelovelove           lovelovelovelove\n");
+    printf("   lovelovelovelovelove       lovelovelovelovelove\n");
+    printf("  lovelovelovelovelovelo     lovelovelovelovelovelo\n");
+    printf(" lovelovelovelovelovelove   lovelovelovelovelovelov\n");
+
+    for (i = 0; i < 2; i++)
+    {
+        printf("lovelovelovelovelovelovelovelovelovelovelovelovelove\n");
+
+    }
+
+    for (i = 0; i < 5; i++)      //爱心的中间部分的上部分
+    {
+        y = 50;
+        y = y - i *2;
+        n++;
+        for (k = 0; k < n; k++)
+        {
+            printf(" ");
+        }
+        while (1)
+        {
+            if (x < y)
+            {
+                printf("1");
+                y--;
+            }
+            else
+                break;
+            if (x < y)
+            {
+                printf("o");
+                y--;
+            }
+            else
+                break; if (x < y)
+            {
+                printf("e");
+                y--;
+            }
+                else
+                break;
+        }
+        printf("\n");
+    }
+
+
+
+
+    //最下面的部分，具体内容同上，没和上一部分放一起是因为从这行开始多两个空格
+
+    for (i = 0, n = 3; i < 10; i++)
+    {
+        y = 37;
+        y = y - i * 4;
+        n++;
+        for (k = 0; k < n; k++)
+        {
+            printf("  ");
+        }
+        while (1)
+        {
+            if (x < y)
+            {
+                printf("l");
+                y--;
+            }
+            else
+                break;
+            if (x < y)
+            {
+                printf("o");
+                y--;
+            }
+            else
+                break; if (x < y)
+            {
+                printf("v");
+                y--;
+            }
+                else
+                break; if (x < y)
+            {
+                printf("e");
+                y--;
+            }
+                else
+                break;
+        }
+        printf("\n");
+    }
+
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n");
+    return 0;
+}
